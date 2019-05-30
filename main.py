3704f1304efdd8fb62edb82e7db750148e7c13d2
jesn1219 <jesn1219@gmail.com>
2019-05-30 14:18:34 +0900

    Applying idf

--- main.py
+++ main.py
@@ -14,9 +14,52 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 from excel_write import *
+import math
 
 
+# return 
+def test_getSimilarity(path_input) :
+    indexed_inputSet = indexingTestFiles(path_input)
+    queryModelSet = []
+    for queryModel in indexed_inputSet :
+        queryModelSet.append(make_and_scale_queryModel(queryModel, dataSource.modelFrame, dataSource))
 
+    similarityResultSet = calGenreSimilarity(dataSource.modelSet, queryModelSet)
+    return similarityResultSet
+
+def test_getPrecision(similSet, rankRange) :
+    precisionResult = [] 
+    for sR  in similSet :
+        name_tmp = sR[0]
+        # [ '타이틀' '번위 이내 맞춘 빈도' '실제 장르' '정확도 예측 리스트' '맞춘 것만 리스트' ]
+        precisionResult_unit = [sR[0], 0 , labeledGenre(name_tmp), sR[1][:], []  ] 
+        #[ 'title', 잘 예측한 갯수, [랭킹값, 장르], 원래 장르 ]
+        genreLabels_tmp = labeledGenre(name_tmp) 
+
+        for rankIdx in range(rankRange) :
+            genre_tmp = sR[1][rankIdx][0]
+            if genre_tmp in  genreLabels_tmp :
+                precisionResult_unit[4].append([rankIdx+1,genre_tmp])
+                precisionResult_unit[1] += 1
+        precisionResult.append(precisionResult_unit); 
+    return precisionResult
+
+
+def testify(path_input) :
+    similSet = test_getSimilarity(path_input)
+    
+    precision_of_testSet = test_getPrecision(similSet, 3)
+    testSize = len(precision_of_testSet)
+    precisionRate = 0
+    for i in precision_of_testSet :
+        print("%30s : %2d ; %s" %(i[0], i[1], i[2]))
+        precisionRate += i[1]
+    precisionRate = precisionRate /  testSize
+    print("Average Val : %f" % precisionRate) 
+    print_and_save_result_to_excel(precision_of_testSet)
+    return precision_of_testSet
+
+# calGenreSimilarity(dataSource.modelSet, )
 # Flag class
 class Flags :
     def __init__(self) :
@@ -29,8 +72,10 @@ class DataReposit :
     # indexed data : [ ['Genre', ['Title', [ 
     indexedSet = []
     modelFrame = []
+    modelIdf = {}
     modelSet = []
     queryModel = []
+     
 
     # Constructor
     def __init__(self) :
@@ -41,7 +86,7 @@ class DataReposit :
         self.indexedSet = indexingScripts()
     
     # 벡터 공간을 만듬. 만들어진 벡터 공간 틀을 이용해 나중에 장르 벡터, 쿼리 벡터들을 만든다. 
-    def createModelFrame(self) :
+    def create_modelFrame(self) :
         self.modelFrame = []
         for g in self.indexedSet :
             # indexedSet 안의 인덱싱 된 모델 g
@@ -55,8 +100,11 @@ class DataReposit :
                     # 틀에 단어 추가
                     self.modelFrame.append([t[0], 0])
     
-    # 인덱스된 셋들을 가지고 벡터를 만든다.
-    def makeModel(self) :
+
+
+
+    # 인덱스된 셋들을 가지고 장르 벡터 모델를 만든다. idf 딕셔너리도 만든다.
+    def make_genreModel_and_idfDic(self) :
         for g in self.indexedSet :
             # g : 인덱싱 된 장르 모델
             # g : ['Genre Name', [ (term, tf), (term2, tf), ..... ]
@@ -69,14 +117,37 @@ class DataReposit :
                 if [word[0],0] in tmpModel[1] :
                     tmpIdx = tmpModel[1].index([word[0],0])
                     tmpModel[1][tmpIdx] = [word[0], word[1]]
-            
+
+                    # idf 딕셔너리의 df 값 만들기
+                    if word[0] in self.modelIdf.keys() :
+                        self.modelIdf[word[0]] += 1
+                    else :
+                        self.modelIdf[word[0]] = 1
+                    
+
             # 만들어진 장르 벡터 모델은 modelSet 리스트에 저장된다
             self.modelSet.append(tmpModel)
+        # df 를 idf로 바꿈
+        # idf setting
+        for term in self.modelIdf :
+            #print(self.modelIdf[term])
+            self.modelIdf[term] = 1 + math.log(self.modelIdf[term] ,2)
+            self.modelIdf[term] = 1 / self.modelIdf[term]
+            #print(self.modelIdf[term])
+              
+
+
+
 
     # modelSet을 스케일링.
-    def scaleModelSet(self,size) :
+    def scale_ModelSet(self,size) :
         modelScaler(self.modelSet,size)
     
+    def applying_idf_to_genreModel(self) :
+        for model in self.modelSet :
+            applying_idf_to_model(self.modelIdf,model)
+
+
     # 장르 추측해내기.
     def predictGenre(self) :
         
@@ -115,12 +186,11 @@ class DataReposit :
             y = [i[1] for i in self.queryModel[0][1][:]]
             axQ.bar(x,y, width = 1.0, color = "red")
             axQ.set_title('Query : %s' % self.queryModel[0][0])
-            axQ.xaxis.set_major_formatter(plt.NullFormatter())
+            #axQ.xaxis.set_major_formatter(plt.NullFormatter())
             #axQ.yaxis.set_major_locator(plt.NullLocator())
         except (IndexError) : 
             # 쿼리 벡터가 없을 때 except
             print('')
-        
 
         plt.subplots_adjust(hspace = 1.0, wspace = 0.1)
         plt.show()
@@ -158,7 +228,11 @@ def modelScaler(modelSet,scale) :
         
         for term in model[1] :
             term[1] = term[1] * 100/total_tf
-
+def applying_idf_to_model(idfDic, model) :
+    retModel = model[:]
+    for termData in retModel[1] :
+        termData[1] = termData[1] * idfDic[termData[0]]
+    return retModel
 
 # 쿼리 날리기
 # @param[in] modelFrame : use to make query vector
@@ -186,7 +260,7 @@ def querying(modelFrame, flags) :
     
     print("Query Processing.. : %s " % nameBuf)
     indexedQuery = queryProcessing(text,nameBuf)
-    queryModel = make_model_to_query(indexedQuery, modelFrame)
+    queryModel = make_and_scale_queryModel(indexedQuery, modelFrame, dataSource)
     return queryModel
 
 
@@ -206,17 +280,22 @@ def calModelDistance(genreVector, queryVector) :
 # make_queryModel (indexedQuerySet, modelFrame) 
 #
 
-def make_and_scale_queryModel (indexedQuerySet, modelFrame) :
+def make_and_scale_queryModel (indexedQuery, modelFrame, ds) :
     queryModelSet = []
-    for indexedQuery in indexedQuerySet[:] :
-        queryModel_tmp = [ indexedQuery[0], modelFrame[:]] 
-        for word in indexedQuery[1] :
-            if [ word[0],0 ] in queryModel_tmp[1] :
-                tmpIdx = queryModel_tmp[1].index([word[0],0])
-                queryModel_tmp[1][tmpIdx] = [word[0], word[1]]
+
+    queryModel_tmp = [ indexedQuery[0], modelFrame[:]] 
+    for word in indexedQuery[1] :
+        if [ word[0],0 ] in queryModel_tmp[1] :
+            tmpIdx = queryModel_tmp[1].index([word[0],0])
+            queryModel_tmp[1][tmpIdx] = [word[0], word[1]]
+        
         queryModelSet.append(queryModel_tmp) 
     modelScaler(queryModelSet, 1000)
-    return queryModelSet
+    
+    
+    queryModel = applying_idf_to_model(ds.modelIdf , queryModelSet[0])
+    
+    return queryModel
 
 # 삭제 예정, 위의 것으로 대체 
 def make_model_to_query(indexedQuery, modelFrame) :
@@ -228,6 +307,7 @@ def make_model_to_query(indexedQuery, modelFrame) :
             tmpIdx = tmpModel[1].index([word[0],0])
             tmpModel[1][tmpIdx] = [word[0], word[1]]
     modelScaler([tmpModel],1000)
+
 
     return tmpModel
 
@@ -254,22 +334,26 @@ def labeledGenre(name) :
 
 
 
-print("Hello")
 
 
 # makeNewModelData :
 # True 이면, 장르 벡터 모델을 생성한다. 
 # False이면, 계산하여 저장해 놓은 모델을 불러온다 indexedModel.t2m 으로 저장되어있음
 
-makeNewModelData = False 
+makeNewModelData = False
 dataSource = DataReposit()
+path_testifyResult = "./"
+path_data = "./data"
+flags = Flags()
+
+print("Hello")
 
 if makeNewModelData :
     # 장르 벡터 모델 생성
     dataSource = DataReposit()
     dataSource.indexing()
-    dataSource.createModelFrame()
-    dataSource.makeModel()
+    dataSource.create_modelFrame()
+    dataSource.make_genreModel_and_idfDic()
     
     # 만든 장르 벡터 모델 저장 
     with open("savedModel.t2m", "wb") as file :
@@ -279,54 +363,16 @@ else :
     # 장르 벡터 모델 불러오기 
     with open("savedModel.t2m", "rb") as file :
         dataSource = pickle.load(file)
-        dataSource.makeModel()
+        dataSource.make_genreModel_and_idfDic()
+# test
+dataSource.create_modelFrame()
+
 # 장르 벡터 모델 스케일링
-dataSource.scaleModelSet(1000)
+dataSource.scale_ModelSet(1000)
+dataSource.applying_idf_to_genreModel()
 
 
 path_testdir = "./input"
-
-# return 
-def test_getSimilarity(path_input) :
-    indexed_inputSet = indexingTestFiles(path_input)
-    queryModelset = make_and_scale_queryModel(indexed_inputSet, dataSource.modelFrame)
-    similarityResultSet = calGenreSimilarity(dataSource.modelSet, queryModelset)
-    return similarityResultSet
-
-def test_getPrecision(similSet, rankRange) :
-    precisionResult = [] 
-    for sR  in similSet :
-        name_tmp = sR[0]
-        # [ '타이틀' '번위 이내 맞춘 빈도' '실제 장르' '정확도 예측 리스트' '맞춘 것만 리스트' ]
-        precisionResult_unit = [sR[0], 0 , labeledGenre(name_tmp), sR[1][:], []  ] 
-        #[ 'title', 잘 예측한 갯수, [랭킹값, 장르], 원래 장르 ]
-        genreLabels_tmp = labeledGenre(name_tmp) 
-
-        for rankIdx in range(rankRange) :
-            genre_tmp = sR[1][rankIdx][0]
-            if genre_tmp in  genreLabels_tmp :
-                precisionResult_unit[4].append([rankIdx+1,genre_tmp])
-                precisionResult_unit[1] += 1
-        precisionResult.append(precisionResult_unit); 
-    return precisionResult
-
-
-def testify(path_input) :
-    similSet = test_getSimilarity(path_input)
-    
-    precision_of_testSet = test_getPrecision(similSet, 3)
-    testSize = len(precision_of_testSet)
-    precisionRate = 0
-    for i in precision_of_testSet :
-        print("%30s : %2d ; %s" %(i[0], i[1], i[2]))
-        precisionRate += i[1]
-    precisionRate = precisionRate /  testSize
-    print("Average Val : %f" % precisionRate) 
-    print_and_save_result_to_excel(precision_of_testSet)
-    return precision_of_testSet
-
-# calGenreSimilarity(dataSource.modelSet, )
-flags = Flags()
 
 # querying
 while (True) :
@@ -353,8 +399,6 @@ while (True) :
         print("")
         #dataSource.showModel(True)
 
-path_testifyResult = "./"
-path_data = "./data"
 
 # testify all the scripts in input Folder
 # @param [in]   datasource
